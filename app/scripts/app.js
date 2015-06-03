88a4b70b2f63d3a669678dab4a885e26d4cca33c
Mirko Terrazas <mirko.jterrazasv@gmail.com>
2015-06-03 13:30:20 -0400

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name longCalculatorApp
+ * @description
+ * # longCalculatorApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('longCalculatorApp', [
+    'ngRoute'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name longCalculatorApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the longCalculatorApp
+ */
+angular.module('longCalculatorApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
