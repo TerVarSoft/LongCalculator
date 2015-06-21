ad40d85607ae647e381c66790f26433ed0b74ec5
Wilmer Terrazas <wilmer.gtv.96@gmail.com>
2015-06-21 15:16:29 -0400

    WindowsPartQuantity  :  Added subidvision for the number of the parts of the sticks
    Added subidvision for the number of the parts of the sticks so the user can add more longs just with one value.

--- app/scripts/controllers/windowsCtrl.js
+++ app/scripts/controllers/windowsCtrl.js
@@ -2,37 +2,56 @@
 
 angular.module('longCalculatorApp')
   .controller('windowsCtrl', function ($scope, $http, windowsInfo) {
-    var removeTemplate = '<span class="glyphicon glyphicon-trash" ng-click="grid.appScope.removeRow(row)" />';
-
-    $scope.mySelections = [];
+    var removeTemplate = '<i class="glyphicon glyphicon-trash" ng-click="removeRow(row)" />';
+    var longColumnsWidth = 70;
+    var quantityColumnsWidth = 40;
     
     var windowsData = windowsInfo.getWindows();
     
     $scope.gridOptions = {
-        data: windowsData,
-        enableCellSelection: true,
-        enableCellEdit: true,
-        enableRowSelection: false,
-        enableColumnResize: true,
-        
-        columnDefs: [{field: 'remove', displayName: '', cellTemplate: removeTemplate},
-                     {field: 'name', displayName: 'Ventana', width: 200},
-                     {field: 'rielSuperior', displayName:'Riel Superior'},
-                     {field: 'rielInferior', displayName:'Riel Inferior'},
-                     {field: 'jamba', displayName:'Jamba'},
-                     {field: 'gancho', displayName:'Gancho'},
-                     {field: 'cabezal', displayName:'Cabezal'},
-                     {field: 'socalo', displayName:'Socalo'}]
-        };
+        columnDefs: [{field: 'remove', headerName: '', group: 'Ventanas', template: removeTemplate, width: 30},
+                     {field: 'name', headerName: 'Ventana', group: 'Ventanas', editable: true, width: 150},
+                     {field: 'rielSuperior', headerName:'Longitud', group: 'Riel Superior', editable: true, width: longColumnsWidth},
+                     {field: 'rielSuperiorQuantity', headerName:'Nº', group: 'Riel Superior', editable: true, width: quantityColumnsWidth},
+                     {field: 'rielInferior', headerName:'Longitud', group: 'Riel Inferior', editable: true, width: longColumnsWidth},
+                     {field: 'rielInferiorQuantity', headerName:'Nº', group: 'Riel Inferior', editable: true, width: quantityColumnsWidth},
+                     {field: 'jamba', headerName:'Longitud', group: 'Jamba', editable: true, width: longColumnsWidth},
+                     {field: 'jambaQuantity', headerName:'Nº', group: 'Jamba', editable: true, width: quantityColumnsWidth},
+                     {field: 'gancho', headerName:'Longitud', group: 'Gancho', editable: true, width: longColumnsWidth},
+                     {field: 'ganchoQuantity', headerName:'Nº', group: 'Gancho', editable: true, width: quantityColumnsWidth},
+                     {field: 'cabezal', headerName:'Longitud', group: 'Cabezal', editable: true, width: longColumnsWidth},
+                     {field: 'cabezalQuantity', headerName:'Nº', group: 'Cabezal', editable: true, width: quantityColumnsWidth},
+                     {field: 'socalo', headerName:'Longitud', group: 'Socalo', editable: true, width: longColumnsWidth},
+                     {field: 'socaloQuantity', headerName:'Nº', group: 'Socalo', editable: true, width: quantityColumnsWidth}],
+   
+        rowData: windowsData,
+        angularCompileRows: true,
+        groupHeaders: true,
+    };
     
     $scope.addNewWindow = function() {
-        windowsData.push({name: 'Ventana '+windowsInfo.windowNumber, jamba: '', rielSuperior: '', rielInferior: ''});
+        windowsData.push({name: 'Ventana '+windowsInfo.windowNumber,
+                          rielSuperior: '',
+                          rielSuperiorQuantity: '',
+                          rielInferior: '',
+                          rielInferiorQuantity: '',
+                          jamba: '',
+                          jambaQuantity: '',
+                          gancho: '',
+                          ganchoQuantity: '',
+                          cabezal: '',
+                          cabezalQuantity: '',
+                          socalo: '',
+                          socaloQuantity: ''});
+        
         windowsInfo.incrementNumberWindow();
+        $scope.gridOptions.api.onNewRows();
     };
     
     $scope.removeRow = function(row){
-      var index = $scope.gridOptions.data.indexOf(row.entity);
-            $scope.gridOptions.data.splice(index, 1);
+        var index = $scope.windowsData.indexOf(row.entity);
+        windowsData.splice(1, 1);
+//        $scope.gridOptions.api.onNewRows();
     }
     
     $scope.saveData = function() {
