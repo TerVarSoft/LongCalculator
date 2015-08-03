3cdcfcc6b9749c2202469132d717ba762761cfd9
Wilmer Terrazas <wilmer.gtv.96@gmail.com>
2015-08-03 15:21:33 -0400

    ImprovedCalculator : Improved calculator
    Improved performance of calculator for a better optimization of the cuts

--- app/scripts/controllers/calculatorCtrl.js
+++ app/scripts/controllers/calculatorCtrl.js
@@ -3,6 +3,7 @@
 
 angular.module('longCalculatorApp')
   .controller('calculatorCtrl', function ($scope, windowsInfo, LongsSorter) {
+
     $scope.longValue = '';
     $scope.windowsPartsNames = windowsInfo.getWindowsPartsNames();
     $scope.selectedWindowPart = $scope.windowsPartsNames[0];
@@ -21,28 +22,33 @@ angular.module('longCalculatorApp')
     var sortedLongs = [];
     
     $scope.windowPartDetails = function(windowPart) {
-        $scope.sticksSizes = windowsInfo.getSticksSizes(windowPart);
-        $scope.longObjects = windowsInfo.getLongs(windowPart);
-        sortLongs();
-        recalculateDetails();
+        updateSticksDetails(windowPart);
         sortedLongs = [];
     }
 
 
     $scope.refreshCanvas = function(){
-        $scope.sticksSizes = windowsInfo.getSticksSizes($scope.selectedWindowPart);
+        updateSticksDetails($scope.selectedWindowPart);
+        repaintCanvas();
+        sortedLongs = [];
+    }
+    
+    function updateSticksDetails(windowPart) {
+        $scope.sticksSizes = windowsInfo.getSticksSizes(windowPart);
+        var defaultStickSize = $scope.sticksSizes[0];
+        $scope.sticksSizes.splice(0, 1);
+        $scope.sticksSizes = _.filter($scope.sticksSizes, function (stickSize) { return stickSize != 0});
         $scope.sticksSizes.sort(function(stick1, stick2) {
             return stick1 - stick2;
         });
-        $scope.longObjects = windowsInfo.getLongs($scope.selectedWindowPart);
+        $scope.sticksSizes.push(defaultStickSize);
+        $scope.longObjects = windowsInfo.getLongs(windowPart);
         $scope.longRows = _.pluck($scope.longObjects,'value');
         
         sortedLongs = LongsSorter.sortLongs($scope.longObjects, $scope.sticksSizes);
         
         if(!sortedLongs) sortedLongs = [];
         recalculateDetails();
-        repaintCanvas();
-        sortedLongs = [];
     }
     
     function recalculateDetails(){
@@ -60,16 +66,29 @@ angular.module('longCalculatorApp')
         
         $scope.sticksDetails.push({
             numberOfStick : 1,
-            longStick: $scope.sticksSizes[0],
+            stickLong: $scope.sticksSizes[0],
             values : [],
-            lostMilimeters: 0
+            lostMilimeters: $scope.sticksSizes[0] 
           });
+
+        if(sortedLongs[0].value > $scope.sticksSizes[0]) {
+            $scope.sticksDetails[0].values.push({
+                    name: "-----",
+                    value: "-----"
+            });
+        }
         
         while(i < sortedLongs.length) {
             tempSum += parseFloat(sortedLongs[i].value);
             
-            if(tempSum > $scope.sticksSizes[iSticksSizes]){
-                tempSum = parseFloat(sortedLongs[i].value);
+            if(tempSum <= $scope.sticksSizes[iSticksSizes]) {
+                $scope.sticksDetails[numberOfSticks-1].values.push(sortedLongs[i]);
+                $scope.sticksDetails[numberOfSticks-1].lostMilimeters = ($scope.sticksSizes[iSticksSizes] - tempSum).toFixed(1);
+                i++;
+            }
+            
+            else{
+                tempSum = 0;
                 numberOfSticks++;
                 
                 if(iSticksSizes < ($scope.sticksSizes.length-1)) {
@@ -78,58 +97,106 @@ angular.module('longCalculatorApp')
                 
                 $scope.sticksDetails.push({
                     numberOfStick : numberOfSticks,
-                    longStick: $scope.sticksSizes[iSticksSizes],
+                    stickLong: $scope.sticksSizes[iSticksSizes],
                     values : [],
-                    lostMilimeters: 0
+                    lostMilimeters: $scope.sticksSizes[iSticksSizes]
                 });
-            }
+            }            
             
-            if(tempSum < $scope.sticksSizes[iSticksSizes]) {
-                $scope.sticksDetails[numberOfSticks - 1].values.push(sortedLongs[i]);
-                $scope.sticksDetails[numberOfSticks - 1].lostMilimeters = ($scope.sticksSizes[iSticksSizes] - tempSum).toFixed(1);
-                i++;
-            }
         }
     }
     
     function repaintCanvas(){
         var sticksCanvas = document.getElementById('sticksCanvas');
-        var context = sticksCanvas.getContext('2d');
+        var stage = new createjs.Stage("sticksCanvas");
         var iSticksSizes = 1;
         $scope.canvasData.lastXposition = 5;
         $scope.canvasData.accumulatedMilimeters = 0;
         $scope.canvasData.numberOfSticks = 1;
-        context.clearRect(0, 0, sticksCanvas.width, sticksCanvas.height);
- 
-        paintSticks(context);       
-    }
+        //context.clearRect(0, 0, sticksCanvas.width, sticksCanvas.height);
         
-        function paintSticks(context) {
-            for(var i = 0; i < $scope.sticksDetails.length; i++){
-                $scope.canvasData.lastXposition = 5;
-                var stickDetails = $scope.sticksDetails[i];
-                var numberOfStick = stickDetails.numberOfStick;
-                var longStick = stickDetails.longStick;
-                var maxLongStick = $scope.sticksSizes[$scope.sticksSizes.length-1];
-                
-                context.strokeStyle='black';
-                context.strokeRect(5, 25 * numberOfStick, (600*longStick) / maxLongStick, 10); 
+        var longTextLabel = new createjs.Text("Longitud", '12px Arial', '#821B2C');
+        longTextLabel.x = 5;
+        longTextLabel.y = 5;
+        var cutsTextLabel = new createjs.Text("Cortes de barras", '12px Arial', '#821B2C');
+        cutsTextLabel.x = 300;
+        cutsTextLabel.y = 5;
+        
+        //new Opentip("#sticksCanvas", "Shown after 2 seconds", { delay: 0 });
+        //this.longTextLabel.opentip("Shown after 2 seconds", { delay: 2 });
+        
+        stage.addChild(longTextLabel);
+        stage.addChild(cutsTextLabel);
+        paintSticks(stage);       
+    }
+    
+    function paintSticks(stage) {
+        for(var i = 0; i < $scope.sticksDetails.length; i++){
+            $scope.canvasData.lastXposition = 70;
+            var stickDetails = $scope.sticksDetails[i];
+            var numberOfStick = stickDetails.numberOfStick;
+            var stickLong = stickDetails.stickLong;
+            var maxLongStick = _.max($scope.sticksSizes, function(sticksSizes) { return sticksSizes });
+            
+            var longLabel = new createjs.Text(stickDetails.stickLong, '12px Arial', '#241C69');
+            longLabel.x = 5;
+            longLabel.y = 25 * numberOfStick;
+            stage.addChild(longLabel);
+            
+            var stickShape = new createjs.Shape();
+            stickShape.graphics.beginStroke("#000000").drawRect(70, 25 * numberOfStick, (600*stickLong) / maxLongStick, 10);
+            stickShape.graphics.endStroke();
+            stage.addChild(stickShape);
 
-                if(stickDetails.values.length > 0) {
-                    for(var j = 0; j < stickDetails.values.length; j++) {
-                        var longValue = stickDetails.values[j].value;
-                        context.fillRect($scope.canvasData.lastXposition, 25 * numberOfStick, (longValue*600) / maxLongStick, 10);
-                        $scope.canvasData.lastXposition += ((longValue*600) / maxLongStick);
-                        
-                        context.beginPath();
-                        context.moveTo($scope.canvasData.lastXposition,(25 * numberOfStick) - 10);
-                        context.lineTo($scope.canvasData.lastXposition,(25 * numberOfStick) + 20);
-                        context.strokeStyle='red';
-                        context.stroke();
+
+            if(stickDetails.values.length > 0) {
+                for(var j = 0; j < stickDetails.values.length; j++) {
+                    var longValue = stickDetails.values[j].value;
+                    var longShape = new createjs.Shape();
+                    longShape.graphics.beginFill("#C4BEBF")
+                             .drawRect($scope.canvasData.lastXposition, 25 * numberOfStick, (longValue*600)/maxLongStick, 10);
+                    $scope.canvasData.lastXposition += ((longValue*600) / maxLongStick);
+                    
+                    stage.enableMouseOver();
+
+                    longShape.onMouseOver = function(e) {
+                        stage.canvas.title = 'put your tooltip text here';
+                        console.log('put your tooltip text here');
                     }
+
+                    longShape.onMouseOut = function(e) {
+                        stage.canvas.title = '';
+                        console.log('p');
+                    }
+                    stage.addChild(longShape);
+
+                    var separatorShape = new createjs.Shape();
+                    separatorShape.graphics.setStrokeStyle(1,"round");
+                    separatorShape.graphics.beginStroke("#FF0000").moveTo($scope.canvasData.lastXposition,(25 * numberOfStick) - 5);
+                    separatorShape.graphics.lineTo($scope.canvasData.lastXposition,(25 * numberOfStick) + 15);
+                    separatorShape.graphics.endStroke();
+                    stage.addChild(separatorShape);
                 }
             }
         }
+        
+        stage.update();
+    }
+    
+    /*function repaintCanvas(){
+        var sticksCanvas = document.getElementById('sticksCanvas');
+        var stage = new createjs.Stage("sticksCanvas");
+        
+        var text = new createjs.Text('Hello, World!', '32px Arial', '#53f');
+        text.textAlign = 'center';
+        text.x = sticksCanvas.width/2;
+        text.y = sticksCanvas.height/2;
+
+        stage.addChild(text);
+
+        stage.update();
+    }*/
+    
  
     $scope.refreshCanvas();
   });
